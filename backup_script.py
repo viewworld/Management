e5fd6e3c6df0a45b36139a36bbde1189a302cd76
runepersson <rbp@viewworld.dk>
2012-01-17 14:54:59 +0100

    added daily backup of system disk on production server

--- backup_script.py
+++ backup_script.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python
 
-# Backup script for taking daily snapshots of data disk on production server. 
+# Backup script for taking daily snapshots of data disk on production server.
 # Must be run as a nightly cron job from trac.viewworld.dk
 # This script only backs up the data-disk containing reports, media files are stored in S3
 # TODO: Make sure that disk is in a fixed state before a snapshot is taken
 
-import os 
-from datetime import date  
-  
-pem_file = '/home/ec2-user/pk-QH4XHF6HOJOUXGIT6FGEDB3HYYDMK7RW.pem'  
-cert_file = '/home/ec2-user/cert-QH4XHF6HOJOUXGIT6FGEDB3HYYDMK7RW.pem'  
+import os
+from datetime import date
 
-data_disk = 'vol-9e14b9f7'
-ec2_path = '/opt/aws/bin/' #use trailing slash  
+pem_file = '/root/pk-GBNTSOEAWSSARGIMFSKIIVP33LJ2TE3J.pem'
+cert_file = '/root/cert-GBNTSOEAWSSARGIMFSKIIVP33LJ2TE3J.pem'
+
+data_disk = 'vol-bedd9fd7'
+sys_disk = 'vol-ca0c43a3'
+ec2_path = '/usr/bin/' #use trailing slash
 region = 'eu-west-1'
 
 #SCRIPTING BELOW
 
-days = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')  
-man_day = days[date.today().weekday()]  
+days = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
+man_day = days[date.today().weekday()]
 man_date = date.today().isoformat()
 
-ct_snap = '%sec2-create-snapshot %s -K %s -C %s --region %s -d "Production Data Backup %s-%s"' % (ec2_path, data_disk, pem_file, cert_file, region, man_day, man_date)
+ct_data_snap = '%sec2-create-snapshot %s -K %s -C %s --region %s -d "Production Data Backup %s-%s"' % (ec2_path, data_disk, pem_file, cert_file, region, man_day, man_date)
 
-print ct_snap 
-os.system(ct_snap)  
+print ct_data_snap
+os.system(ct_data_snap)
+
+ct_sys_snap = '%sec2-create-snapshot %s -K %s -C %s --region %s -d "Production System Backup %s-%s"' % (ec2_path, sys_disk, pem_file, cert_file, region, man_day, man_date)
+
+print ct_sys_snap
+os.system(ct_sys_snap)
